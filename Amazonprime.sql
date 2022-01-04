7d8a7532a71e1fb3494b7964741875ba2befbffd
Rik01 <rikch01@gmail.com>
2022-01-04 14:20:12 +0100

    Initial

---
+++ Amazonprime.sql
@@ -0,0 +1,10 @@
+/*
+Amazonprime Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+Select *
+From PortfolioProject..Amazonprime
+where type is not NULL
+order by 2,3
